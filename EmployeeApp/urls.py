965e74564552499d4bce0c1bdff3e9c468e44491
unleasha123 <ranjan.mishra088@gmail.com>
2022-01-11 20:59:36 +0530

    Testing python API code

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from EmployeeApp import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns=[
+    url(r'^department$',views.departmentAPI),
+    url(r'^department/([0-9]+)$',views.departmentAPI),
+
+    url(r'^employee$',views.employeeAPI),
+    url(r'^employee/([0-9]+)$',views.employeeAPI),
+
+    url(r'^employee/savefile',views.SaveFile)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
